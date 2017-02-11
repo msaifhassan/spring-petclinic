09159ed4664ae6ce31b2c9a4ca45a9bbbf26d861
Mohammed S Hassan <msaifhassan@gmail.com>
2017-02-11 13:30:36 -0600

    Added spock tests and updated sonar properties

--- src/test/groovy/org/springframework/samples/petclinic/model/ValidatorSpockTests.groovy
+++ src/test/groovy/org/springframework/samples/petclinic/model/ValidatorSpockTests.groovy
@@ -1,18 +1,12 @@
-package org.springframework.samples.petclinic.model;
+package org.springframework.samples.petclinic.model
 
-//import static org.assertj.core.api.Assertions.assertThat;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean
+import spock.lang.Specification
 
-import java.util.Locale;
-import java.util.Set;
-
-import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolation
 import javax.validation.Validator;
 
-import org.springframework.context.i18n.LocaleContextHolder;
-import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
-
-import spock.lang.Specification;
-
+//import static org.assertj.core.api.Assertions.assertThat;
 /**
  * @author Chris Jones
  *         Simple tests adapted to use the Spock acceptance test framework.
@@ -43,13 +37,86 @@ public class ValidatorSpockTests extends Specification {
 	  violation.getMessage().equals("may not be empty");
 	}
 	
-	// 2. TODO: validate against null first name.
-	
-	// 3. TODO: validate a against a valid first name (non-empty, non-null value).
+	// 2. validate against null first name.
+    def "first name cannot be null"() {
+        setup:
+        def person = new Person();
+        def validator = createValidator();
 
-	// 4. TODO: validate against empty last name
+        when:
+        person.setFirstName(null)
+        person.setLastName("smith")
+        Set<ConstraintViolation<Person>> constraintViolations = validator.validate(person);
 
-	// 5. TODO: validate against null last name
+        then:
+        constraintViolations.size() == 1;
+        ConstraintViolation<Person> violation = constraintViolations.iterator().next();
+        violation.getPropertyPath().toString().equals("firstName");
+        violation.getMessage().equals("may not be empty");
+    }
 	
-	// 6. TODO: validate a against a valid last name (non-empty, non-null value).
+	// 3. validate a against a valid first name (non-empty, non-null value).
+    def "first name is set properly"() {
+        setup:
+        def person = new Person();
+        def validator = createValidator();
+
+        when:
+        person.setFirstName("steve")
+        person.setLastName("smith")
+        Set<ConstraintViolation<Person>> constraintViolations = validator.validate(person);
+
+        then:
+        person.firstName.equals("steve")
+    }
+
+	// 4. validate against empty last name
+    def "last name cannot be empty"() {
+        setup:
+        def person = new Person();
+        def validator = createValidator();
+
+        when:
+        person.setFirstName("steve")
+        person.setLastName("")
+        Set<ConstraintViolation<Person>> constraintViolations = validator.validate(person);
+
+        then:
+        constraintViolations.size() == 1;
+        ConstraintViolation<Person> violation = constraintViolations.iterator().next();
+        violation.getPropertyPath().toString().equals("lastName");
+        violation.getMessage().equals("may not be empty");
+    }
+
+	// 5. validate against null last name
+    def "last name cannot be null"() {
+        setup:
+        def person = new Person();
+        def validator = createValidator();
+
+        when:
+        person.setFirstName("steve")
+        person.setLastName(null)
+        Set<ConstraintViolation<Person>> constraintViolations = validator.validate(person);
+
+        then:
+        constraintViolations.size() == 1;
+        ConstraintViolation<Person> violation = constraintViolations.iterator().next();
+        violation.getPropertyPath().toString().equals("lastName");
+        violation.getMessage().equals("may not be empty");
+    }
+	
+	// 6. validate a against a valid last name (non-empty, non-null value).
+    def "last name is set properly"() {
+        setup:
+        def person = new Person();
+        def validator = createValidator();
+
+        when:
+        person.setFirstName("steve")
+        person.setLastName("smith")
+
+        then:
+        person.lastName.equals("smith")
+    }
 }
